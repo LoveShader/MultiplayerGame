beaa7a14eccd0742837c9c554caf7360f82f5b80
LoveShader <zy1483480718@gmail.com>
2025-03-16 11:04:52 +0800

    Create Projectile Weapon Class!

---
+++ Source/MultiplayerGame/Private/Weapon/Projectile.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon/Projectile.h"
+
+#include "Components/BoxComponent.h"
+
+AProjectile::AProjectile()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>("CollisionBox");
+	SetRootComponent(CollisionBox);
+
+	CollisionBox->SetCollisionObjectType(ECC_WorldDynamic);
+	CollisionBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	CollisionBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	CollisionBox->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+	CollisionBox->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
+}
+
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
