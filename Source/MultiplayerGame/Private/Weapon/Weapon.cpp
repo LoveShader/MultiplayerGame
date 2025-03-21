e4223b1cb9bdb837daf8cc5ea7b5c93283ffd1bd
LoveShader <zy1483480718@gmail.com>
2025-03-21 23:31:33 +0800

    Bullet Shells!

--- Source/MultiplayerGame/Private/Weapon/Weapon.cpp
+++ Source/MultiplayerGame/Private/Weapon/Weapon.cpp
@@ -6,7 +6,9 @@
 #include "Character/BlasterCharacter.h"
 #include "Components/SphereComponent.h"
 #include "Components/WidgetComponent.h"
+#include "Engine/SkeletalMeshSocket.h"
 #include "Net/UnrealNetwork.h"
+#include "Weapon/Casing.h"
 
 AWeapon::AWeapon()
 {
@@ -118,6 +120,21 @@ void AWeapon::Fire(const FVector& HitTarget)
 	if (FireAnimation)
 	{
 		WeaponMesh->PlayAnimation(FireAnimation, false);
+	}
+
+	if (CasingClass)
+	{
+		const USkeletalMeshSocket* AmmoEjectSocket = WeaponMesh->GetSocketByName(FName("AmmoEject"));
+		if (AmmoEjectSocket)
+		{
+			FTransform SocketTransform = AmmoEjectSocket->GetSocketTransform(WeaponMesh);
+			UWorld* World = GetWorld();
+			if (World)
+			{
+				World->SpawnActor<ACasing>(CasingClass, SocketTransform.GetLocation(),SocketTransform.GetRotation().Rotator());
+			}
+		}
+		
 	}
 }
 
