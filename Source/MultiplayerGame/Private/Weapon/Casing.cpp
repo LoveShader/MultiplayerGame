2d299eaa50397a4f1b96aa49df6b1a754e513dfe
LoveShader <zy1483480718@gmail.com>
2025-03-23 23:35:59 +0800

    Shell Physics

--- Source/MultiplayerGame/Private/Weapon/Casing.cpp
+++ Source/MultiplayerGame/Private/Weapon/Casing.cpp
@@ -3,16 +3,41 @@
 
 #include "Weapon/Casing.h"
 
+#include "Chaos/GeometryParticlesfwd.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+
 ACasing::ACasing()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
 	CasingMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CasingMesh"));
 	SetRootComponent(CasingMesh);
+
+	//Set Casing Physics Properties
+	CasingMesh->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	CasingMesh->SetSimulatePhysics(true);
+	CasingMesh->SetEnableGravity(true);
+	CasingMesh->SetNotifyRigidBodyCollision(true);
+	ShellEjectionImpulse = 10.0f;
 }
 
 void ACasing::BeginPlay()
 {
 	Super::BeginPlay();
+
+	CasingMesh->AddImpulse(GetActorForwardVector() * ShellEjectionImpulse);
+	CasingMesh->OnComponentHit.AddDynamic(this, &ACasing::OnHit);
+}
+
+void ACasing::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse, const FHitResult& Hit)
+{
+
+	if (ShellSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ShellSound,GetActorLocation());
+	}
+	Destroy();
 }
 
